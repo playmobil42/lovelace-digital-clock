63cc38243ae0dfb3b43781b94a9faf45a153abe4
playmobil42 <62788173+playmobil42@users.noreply.github.com>
2022-11-17 23:40:04 +0200

    Support arbitrary CSS styling via config
        Following the pattern from the the Picture Element Card: https://www.home-assistant.io/dashboards/picture-elements/#style
        Add configuration options to allow specifying CSS Styling to each line individually.
    
        This was previously achievable via card-mod, but specifying styling directly makes this
        card much more convenient to use while lowering the bar for new users.

--- src/DigitalClock.ts
+++ src/DigitalClock.ts
@@ -33,66 +33,82 @@ console.info(
 @customElement('digital-clock')
 export class DigitalClock extends LitElement {
     @property({attribute: false}) public hass!: HomeAssistant;
-    @state() private _firstLine = '';
-    @state() private _secondLine = '';
-    @state() private _config?: IDigitalClockConfig;
-    @state() private _interval = 1000;
-    private _intervalId?: number;
+  @state() private _firstLine = '';
+  @state() private _secondLine = '';
+  @state() private _firstLineElement?: HTMLElement;
+  @state() private _secondLineElement?: HTMLElement;
+  @state() private _config?: IDigitalClockConfig;
+  @state() private _interval = 1000;
+  private _intervalId?: number;
 
-    public setConfig(config: IDigitalClockConfig): void {
-        this._config = { ...config };
-        if (this._config.timeFormat)
+  public setConfig(config: IDigitalClockConfig): void {
+    this._config = { ...config };
+    if (this._config.timeFormat)
             this._config.firstLineFormat = this._config.timeFormat;
         if (this._config.dateFormat)
             this._config.secondLineFormat = this._config.dateFormat;
         if (this._config.interval !== this._interval)
             this._interval = this._config.interval ?? 1000;
-    }
 
-    protected shouldUpdate(changedProps: PropertyValues): boolean {
+    this._firstLineElement = this._createStyledElement('first-line', this._config?.firstLineStyle || {});
+    this._firstLineElement.innerText = this._firstLine;
+    this._secondLineElement = this._createStyledElement('second-line', this._config?.secondLineStyle || {});
+    this._secondLineElement.innerText = this._secondLine;
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
         return changedProps.has('_firstLine') || changedProps.has('_secondLine') || changedProps.has('_config') || changedProps.has('hass');
+  }
+
+  public async getCardSize(): Promise<number> {
+    return 3;
+  }
+
+  protected updated(changedProperties: PropertyValues): void {
+    super.updated(changedProperties);
+
+    if (changedProperties.has('_interval')) {
+      this._stopInterval();
+      this._startInterval();
     }
-
-    public async getCardSize(): Promise<number> {
-        return 3;
-    }
-
-    protected updated(changedProperties: PropertyValues): void {
-        super.updated(changedProperties);
-
-        if (changedProperties.has('_interval')) {
-            this._stopInterval();
-            this._startInterval();
-        }
         if (changedProperties.has('_config'))
             this._updateDateTime();
-    }
+  }
 
-    public connectedCallback(): void {
-        super.connectedCallback();
-        this._startInterval();
-    }
+  public connectedCallback(): void {
+    super.connectedCallback();
+    this._startInterval();
+  }
 
-    private _startInterval(): void {
+  private _createStyledElement(className: string, lineStyle: { [name: string]: string }): HTMLElement {
+    const element = document.createElement('span');
+    element.className = className;
+    for (const prop in lineStyle) {
+      element.style.setProperty(prop, lineStyle[prop]);
+    }
+    return element;
+  }
+
+  private _startInterval(): void {
         if (this._intervalId)
             return;
 
-        this._intervalId = window.setInterval(this._updateDateTime.bind(this), this._interval);
-    }
+    this._intervalId = window.setInterval(this._updateDateTime.bind(this), this._interval);
+  }
 
-    private _stopInterval(): void {
+  private _stopInterval(): void {
         if (!this._intervalId)
             return;
-        window.clearInterval(this._intervalId);
-        this._intervalId = undefined;
-    }
+    window.clearInterval(this._intervalId);
+    this._intervalId = undefined;
+  }
 
-    private async _updateDateTime(): Promise<void> {
-        const timeZone = this._config?.timeZone ?? this.hass?.config?.time_zone;
-        const locale = this._config?.locale ?? this.hass?.locale?.language;
+  private async _updateDateTime(): Promise<void> {
+    const timeZone = this._config?.timeZone ?? this.hass?.config?.time_zone;
+    const locale = this._config?.locale ?? this.hass?.locale?.language;
 
-        let dateTime: DateTime = DateTime.local();
-        /* if (!this._config?.useHATime) {
+    let dateTime: DateTime = DateTime.local();
+    /* if (!this._config?.useHATime) {
             dateTime = DateTime.local();
         } else {
             dateTime = DateTime.fromSeconds(await new Promise<number>((resolve) => {
@@ -108,60 +124,68 @@ export class DigitalClock extends LitElement {
         if (locale)
             dateTime = dateTime.setLocale(locale);
 
-        let firstLine: string;
-        let secondLine: string;
+    let firstLine: string;
+    let secondLine: string;
 
         if (typeof this._config?.firstLineFormat === 'string')
             firstLine = dateTime.toFormat(this._config.firstLineFormat);
         else
             firstLine = dateTime.toLocaleString(this._config?.firstLineFormat ?? { hour: '2-digit', minute: '2-digit' });
 
-        if (typeof this._config?.secondLineFormat === 'string')
-            secondLine = dateTime.toFormat(this._config.secondLineFormat);
-        else
+    if (typeof this._config?.secondLineFormat === 'string')
+      secondLine = dateTime.toFormat(this._config.secondLineFormat);
+    else
             secondLine = dateTime.toLocaleString(this._config?.secondLineFormat ?? { weekday: 'short', day: '2-digit', month: 'short' });
 
-        if (firstLine !== this._firstLine)
-            this._firstLine = firstLine;
-        if (secondLine !== this._secondLine)
-            this._secondLine = secondLine;
+    if (firstLine !== this._firstLine) {
+      this._firstLine = firstLine;
+      if (this._firstLineElement) {
+        this._firstLineElement.innerText = firstLine;
+      }
     }
+    if (secondLine !== this._secondLine) {
+      this._secondLine = secondLine;
+      if (this._secondLineElement) {
+        this._secondLineElement.innerText = secondLine;
+      }
+    }
+  }
 
-    public disconnectedCallback(): void {
-        this._stopInterval();
-        super.disconnectedCallback();
-    }
+  public disconnectedCallback(): void {
+    this._stopInterval();
+    super.disconnectedCallback();
+  }
 
-    protected render(): TemplateResult | void {
-        return html`
-            <ha-card>
-                <span class="first-line">${this._firstLine}</span>
-                <span class="second-line">${this._secondLine}</span>
-            </ha-card>
-        `;
-    }
+  protected render(): TemplateResult | void {
+    return html`
+      <ha-card>
+        ${this._firstLineElement}
+        ${this._secondLineElement}
+      </ha-card>
+    `;
+  }
 
-    static get styles(): CSSResult {
-        return css`
-          ha-card {
-            text-align: center;
-            font-weight: bold;
-            padding: 8px 0;
-          }
-          
-          ha-card > span {
-            display: block;
-          }
-          
-          .first-line {
-            font-size: 2.8em;
-            line-height: 1em;
-          }
-          
-          .second-line {
-            font-size: 1.6em;
-            line-height: 1em;
-          }
-        `;
-    }
+  static get styles(): CSSResult {
+    return css`
+      ha-card {
+        text-align: center;
+        font-weight: bold;
+        padding: 8px 0;
+      }
+
+      ha-card > span {
+        display: block;
+      }
+
+      .first-line {
+        font-size: 2.8em;
+        line-height: 1em;
+      }
+
+      .second-line {
+        font-size: 1.6em;
+        line-height: 1em;
+      }
+    `;
+  }
 }
